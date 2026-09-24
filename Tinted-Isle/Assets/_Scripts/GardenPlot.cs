e09f6e6593226cf47984d617d766faac02e90701
Addison-Dunn <addafriend42@gmail.com>
2026-09-24 14:03:09 -0400

    started working on code for garden plots

---
+++ Tinted-Isle/Assets/_Scripts/GardenPlot.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GardenPlot : MonoBehaviour
+{
+    public Sprite covered;
+    public Sprite normal;
+    public Sprite dry1;
+    public Sprite dry2;
+    public Sprite wet1;
+    public Sprite wet2;
+
+    public bool hovered;
+
+
+}
--- Tinted-Isle/Assets/_Scripts/PointAndClick.cs
+++ Tinted-Isle/Assets/_Scripts/PointAndClick.cs
@@ -1,15 +1,39 @@
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PointAndClick : MonoBehaviour
 {
+    public InputActionAsset inputAction;
+
+    private InputAction clickAction;
+
     public Camera mainCamera;
     private void Update()
+    {
+        Point();
+
+        if (clickAction.WasPressedThisFrame())
+        {
+            Click();
+        }
+    }
+
+    private void Awake()
+    {
+        clickAction = InputSystem.actions.FindAction("Click");
+    }
+
+    void Point()
     {
         Vector3 mousePos = Mouse.current.position.ReadValue();
         mousePos = mainCamera.ScreenToWorldPoint(mousePos);
 
         Vector3 direction = (mousePos - mainCamera.transform.position);
-        direction.z += 10;
+        direction.z -= mainCamera.transform.position.z;
         Debug.DrawRay(mainCamera.transform.position, direction, Color.blue);
+    }
+    public void Click()
+    {
+
     }
 }
